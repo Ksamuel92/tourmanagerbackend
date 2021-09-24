6795bc6f0356431ce00d6d5c5f273db4b0df3127
Kyle Samuel <ksamuel92@gmail.com>
2021-09-24 19:49:31 -0400

    add user resources

--- db/migrate/20210924234427_create_users.rb
+++ db/migrate/20210924234427_create_users.rb
@@ -1,9 +1,8 @@
-class CreateUser < ActiveRecord::Migration[6.1]
+class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :email
       t.string :password
-
       t.timestamps
     end
   end
