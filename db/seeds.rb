46cfb61513463a9a836e350da17048e2d68870e1
Kyle Samuel <ksamuel92@gmail.com>
2021-09-28 13:05:25 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 user = User.create(email: "example.com")
 show = user.shows.create(
     venue: "New Brookland Tavern",
     promoter: "Dude Bro",
     email: "dudebro@NBT.com",
-    guarantee: "$1500",
+    guarantee: "1500",
     advanced: "true",
-    merch: "$2000",
+    merch: "2000",
     loadin: "3:30"
 )
 show = user.shows.create(
     venue: "Emo's",
     promoter: "Dude Bro",
     email: "dudebro@NBT.com",
-    guarantee: "$1200",
+    guarantee: "1200",
     advanced: "true",
-    merch: "$2000",
+    merch: "2000",
     loadin: "3:30"
 )
 show = user.shows.create(
     venue: "Three Links",
     promoter: "Dallas Guy",
     email: "DallasGuy@threelinks.com",
-    guarantee: "$1500",
+    guarantee: "1500",
     advanced: "false",
-    merch: "$2000",
+    merch: "2000",
     loadin: "3:30"
 )
 
 
-# create_table "shows", force: :cascade do |t|
-#     t.string "venue"
-#     t.string "promoter"
-#     t.string "email"
-#     t.integer "guarantee"
-#     t.boolean "advanced"
-#     t.integer "merch"
-#     t.time "loadin"
-#     t.datetime "created_at", precision: 6, null: false
-#     t.datetime "updated_at", precision: 6, null: false
-#     t.bigint "users_id", null: false
-#     t.index ["users_id"], name: "index_shows_on_users_id"
-#   end
