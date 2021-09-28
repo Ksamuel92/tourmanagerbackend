e6fb7396d728b6484410313e8f596476ec0cbb41
Kyle Samuel <ksamuel92@gmail.com>
2021-09-27 20:10:29 -0400

    add activerecord serializer

--- db/migrate/20210927225137_add_user_ref_to_show.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToShow < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :shows, :users, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_225137) do
+ActiveRecord::Schema.define(version: 2021_09_27_230850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2021_09_27_225137) do
     t.time "loadin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "users_id", null: false
-    t.index ["users_id"], name: "index_shows_on_users_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_shows_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +35,5 @@ ActiveRecord::Schema.define(version: 2021_09_27_225137) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "shows", "users", column: "users_id"
+  add_foreign_key "shows", "users"
 end
