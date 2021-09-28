ca79957002c55dec652bb03ec2ae5f5c045de4fc
Kyle Samuel <ksamuel92@gmail.com>
2021-09-28 13:05:35 -0400

    update show serializer

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,3 +1,18 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :venue, :promoter, :email, :guarantee, :advanced, :merch, :loadin
 end
+
+
+# create_table "shows", force: :cascade do |t|
+#     t.string "venue"
+#     t.string "promoter"
+#     t.string "email"
+#     t.integer "guarantee"
+#     t.boolean "advanced"
+#     t.integer "merch"
+#     t.time "loadin"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#     t.bigint "users_id", null: false
+#     t.index ["users_id"], name: "index_shows_on_users_id"
+#   end
