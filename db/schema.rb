b590e597f7ae90239ea508b14bf24f5a9204b5e4
Kyle Samuel <ksamuel92@gmail.com>
2021-10-03 11:45:26 -0400

    add city to seed data and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_230850) do
+ActiveRecord::Schema.define(version: 2021_10_03_154035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_230850) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "city"
     t.index ["user_id"], name: "index_shows_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-
-user = User.create(email: "example.com")
+user = User.create(email: 'example.com')
 show = user.shows.create(
-    venue: "New Brookland Tavern",
-    promoter: "Dude Bro",
-    email: "dudebro@NBT.com",
-    guarantee: "1500",
-    advanced: "true",
-    merch: "2000",
-    loadin: "3:30"
+  venue: 'New Brookland Tavern',
+  city: 'Columbia, SC',
+  promoter: 'Dude Bro',
+  email: 'dudebro@NBT.com',
+  guarantee: '1500',
+  advanced: 'true',
+  merch: '2000',
+  loadin: '3:30'
 )
 show = user.shows.create(
-    venue: "Emo's",
-    promoter: "Dude Bro",
-    email: "dudebro@NBT.com",
-    guarantee: "1200",
-    advanced: "true",
-    merch: "2000",
-    loadin: "3:30"
+  venue: "Emo's",
+  promoter: 'Dude Bro',
+  city: 'Austin, TX',
+  email: 'dudebro@NBT.com',
+  guarantee: '1200',
+  advanced: 'true',
+  merch: '2000',
+  loadin: '3:30'
 )
 show = user.shows.create(
-    venue: "Three Links",
-    promoter: "Dallas Guy",
-    email: "DallasGuy@threelinks.com",
-    guarantee: "1500",
-    advanced: "false",
-    merch: "2000",
-    loadin: "3:30"
+  venue: 'Three Links',
+  city: 'Dallas, TX',
+  promoter: 'Dallas Guy',
+  email: 'DallasGuy@threelinks.com',
+  guarantee: '1500',
+  advanced: 'false',
+  merch: '2000',
+  loadin: '3:30'
 )
-
-
