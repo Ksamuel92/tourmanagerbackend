d90fbee9aed1777cda37043d1c671f5a9e60ba91
Kyle Samuel <ksamuel92@gmail.com>
2021-10-05 11:47:57 -0400

    fixed #create in showcontroller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,7 +5,10 @@ class ShowsController < ApplicationController
   end
 
   def create
-    show = Show.create(show_params)
+    # byebug
+    user = User.find_by(email: params[:user][:email])
+    # byebug
+    show = user.shows.create(show_params)
     show.save
     render json: show
   end
@@ -13,6 +16,10 @@ class ShowsController < ApplicationController
   private
 
   def show_params
-    params.require(:show).permit(:venue, :promoter, :email, :city, :guarantee, :advanced, :merch, :loadin)
+    params.require(:show).permit(:venue, :promoter, :email, :city, :guarantee, :advanced, :user, :merch, :loadin)
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
