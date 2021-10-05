76b18e1d6c92bf4d743a1e1c9e792285b4e9f7b0
Kyle Samuel <ksamuel92@gmail.com>
2021-10-05 10:49:10 -0400

    add name to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_154035) do
+ActiveRecord::Schema.define(version: 2021_10_05_144816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_154035) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "shows", "users"
