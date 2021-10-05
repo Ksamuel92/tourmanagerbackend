56be175e2a03f3da3153cf527bcb366a1e1a890b
Kyle Samuel <ksamuel92@gmail.com>
2021-10-05 10:49:00 -0400

    add #create to shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,18 @@
 class ShowsController < ApplicationController
-    def index
-        shows = Show.all
-        render json: shows
-    end
+  def index
+    shows = Show.all
+    render json: shows
+  end
+
+  def create
+    show = Show.create(show_params)
+    show.save
+    render json: show
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:venue, :promoter, :email, :city, :guarantee, :advanced, :merch, :loadin)
+  end
 end
