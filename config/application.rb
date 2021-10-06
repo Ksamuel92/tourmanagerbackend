acbe2c09dd12c0788492d7a29412a9281ee7f5e8
Kyle Samuel <ksamuel92@gmail.com>
2021-10-06 11:19:02 -0400

    remove local time default in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Tourmanagerbackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_record.default_timezone = :local
+    # config.active_record.default_timezone = :local
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
