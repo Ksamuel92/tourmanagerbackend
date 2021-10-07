c06c5b2617e514977d80c21458205024c128fe63
Kyle Samuel <ksamuel92@gmail.com>
2021-10-07 19:20:46 -0400

    change usercontroller#create to find_or_create_by

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(user_params)
+    user = User.find_or_create_by(user_params)
     render json: user
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
